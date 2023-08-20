bcdd5c46a2d8480aee5eb61b5601b86d0c866aeb
Mukesh <135153880+Mukesh-43@users.noreply.github.com>
2023-08-20 11:08:34 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,7 +6,7 @@ const env = require('./environment');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(`mongodb://127.0.0.1:27017/${env.db}`);
+  await mongoose.connect(`mongodb+srv://mukeshaero43:ebt21qM33O6ZjC78@cluster0.ogaqhy5.mongodb.net/?retryWrites=true&w=majority`);
   console.log('MongoDB connected successfully');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
